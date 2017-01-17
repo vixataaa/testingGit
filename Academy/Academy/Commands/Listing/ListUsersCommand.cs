fff57879058e5370376656f2d7aa286ed81e5254
vixataaa <vmetodiev93@gmail.com>
2017-01-17 09:51:26 +0200

    no message

--- Academy/Academy/Commands/Listing/ListUsersCommand.cs
+++ Academy/Academy/Commands/Listing/ListUsersCommand.cs
@@ -1,7 +1,23 @@
 ﻿namespace Academy.Commands.Listing
 {
+    using Academy.Core;
+    using Academy.Core.Contracts;
     class ListUsersCommand
     {
+        public static void Print()
+        {
+            var engine1 = Engine.Instance;
+            foreach (var trainer in engine1.Trainers)
+            {
+                System.Console.WriteLine(trainer);
+            }
+
+            foreach (var student in engine1.Students)
+            {
+                System.Console.WriteLine(student);
+            }
+        }    
+
         // TODO: Implement this
     }
 }
