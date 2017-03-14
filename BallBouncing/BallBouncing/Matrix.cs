dfbc1b2034179471a7b76b7bae5466c6c536a438
vixataaa <vmetodiev93@gmail.com>
2017-03-14 15:29:38 +0200

    no message

---
+++ BallBouncing/BallBouncing/Matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallBouncing
+{
+    public class Matrix
+    {
+        public char[,] coordinateSystem { get; set; }
+
+        public Matrix(int rows, int cols)
+        {
+            this.coordinateSystem = new char[rows, cols];
+            FillCoordinateSystem();
+        }
+
+        private void FillCoordinateSystem()
+        {
+            for (int i = 0; i < this.coordinateSystem.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.coordinateSystem.GetLength(1); j++)
+                {
+                    this.coordinateSystem[i, j] = '.';
+                }
+            }
+        }
+        
+        public void Draw()
+        {
+            for (int i = 0; i < this.coordinateSystem.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.coordinateSystem.GetLength(1); j++)
+                {
+                    Console.Write(this.coordinateSystem[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ BallBouncing/BallBouncing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallBouncing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new Matrix(15, 15);
+            var ball = new Ball(matrix, 'O');
+            ball.xVelocity = 3;
+            ball.yVelocity = 2;
+            
+            while(true)
+            {
+                Console.Clear();
+                ball.Move();
+                matrix.Draw();
+
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
