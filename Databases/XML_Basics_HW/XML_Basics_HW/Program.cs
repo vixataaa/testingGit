5724fcb72ed84bbd08f6ea43ee9bb0cc834a11f3
vixataaa <vmetodiev93@gmail.com>
2017-05-16 20:41:21 +0300

    ASD

---
+++ Databases/XML_Basics_HW/XML_Basics_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Basics_HW
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
